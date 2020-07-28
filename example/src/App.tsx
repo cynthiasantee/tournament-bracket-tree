26ec774b1576f042daadf30cdf314e4218f47bcb
Cynthia Santee <santeecynthia@gmail.com>
2020-07-28 14:55:24 -0700

    Add hidden dummy

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -18,6 +18,36 @@ const App = () => {
       data: {
         img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/This_Is_Us_%28Logo%29.png/500px-This_Is_Us_%28Logo%29.png',
         link: 'https://en.wikipedia.org/wiki/This_Is_Us'
+      },
+      left: {
+        data: {
+          img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/59/This_Is_Us_%28Logo%29.png/500px-This_Is_Us_%28Logo%29.png',
+          link: 'https://en.wikipedia.org/wiki/This_Is_Us'
+        }
+      },
+      right: {
+        data: {
+          img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/NewGirlTitlesS5.jpg/500px-NewGirlTitlesS5.jpg',
+          link: 'https://en.wikipedia.org/wiki/New_Girl'
+        },
+        left: {
+          data: {
+            img: 'https://upload.wikimedia.org/wikipedia/en/0/0d/Title_screen_for_Netflix%27s_Dead_to_Me.png',
+            link: 'https://en.wikipedia.org/wiki/Dead_to_Me_(TV_series)'
+          },
+          left: {
+            data: {
+              img: 'https://upload.wikimedia.org/wikipedia/en/0/0d/Title_screen_for_Netflix%27s_Dead_to_Me.png',
+              link: 'https://en.wikipedia.org/wiki/Dead_to_Me_(TV_series)'
+            }
+          }
+        },
+        right: {
+          data: {
+            img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/NewGirlTitlesS5.jpg/500px-NewGirlTitlesS5.jpg',
+            link: 'https://en.wikipedia.org/wiki/New_Girl'
+          }
+        }
       }
     },
     right: {
@@ -48,7 +78,7 @@ const App = () => {
 
   const mapShowToNode = (show: Show) => {
     return <div>
-      <a href={show.link}><img height="50px" src={show.img} alt="link"/></a>
+      <a href={show.link}><img height="30px" width="90px" src={show.img} alt="link"/></a>
     </div>;
   }
 
